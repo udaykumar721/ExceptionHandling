1c05c321e37a6aa4f216a7852bf199621143f1c3
udaykumar721 <uday56367@gmail.com.com>
2023-09-22 09:31:39 +0530

    [uday] add.ExceptionHandling Demo..

---
+++ ExceptionHandling/ExceptionHandling/DevidedByZeroException.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling
+{
+    internal class DevidedByZeroException
+    {
+        public void DivisionByZero()
+        {
+            int a = 0;
+            //if (a != 0)
+            //{
+            //    int b = 10 / a;
+            //}
+            try
+            {
+                int b = 10 / a;
+
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
+        }
+    }
+}
---
+++ ExceptionHandling/ExceptionHandling/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling
+{
+    //a problem that occurs dring the execution a program is refer to as an exception
+    //it allow to control a passed from one part of the programme to the another exception 
+    //it will prevent the crashing of the programme 
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DevidedByZeroException devidedByZeroException = new DevidedByZeroException();
+            devidedByZeroException.DivisionByZero();
+            Console.ReadLine();
+        }
+    }
+}
